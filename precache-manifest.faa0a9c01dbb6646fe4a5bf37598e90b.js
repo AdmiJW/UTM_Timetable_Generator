f81804b932f0c5be5904bded9968efc5545c6f8e
AdmiJW <jamweg22@gmail.com>
2020-10-29 14:02:41 +0800

    Updates

---
+++ precache-manifest.faa0a9c01dbb6646fe4a5bf37598e90b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beb0b85cb38b17c148335d2c63a87a42",
+    "url": "/UTM_Timetable_Generator/index.html"
+  },
+  {
+    "revision": "5943f222e1e0b87edab3",
+    "url": "/UTM_Timetable_Generator/static/css/main.1b23514f.chunk.css"
+  },
+  {
+    "revision": "a5aa23f55712d41f64c2",
+    "url": "/UTM_Timetable_Generator/static/js/2.62a5bf3d.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/UTM_Timetable_Generator/static/js/2.62a5bf3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5943f222e1e0b87edab3",
+    "url": "/UTM_Timetable_Generator/static/js/main.c9e2bea1.chunk.js"
+  },
+  {
+    "revision": "4365f880ff53e37826fa",
+    "url": "/UTM_Timetable_Generator/static/js/runtime-main.cacfc23b.js"
+  }
+]);
